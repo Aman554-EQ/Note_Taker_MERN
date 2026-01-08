b5dac01729e1fa0070799955a321524bc679071b
Aman554-EQ <mohammed.aman.nsu@gmail.com>
2026-01-08 17:38:52 +0600

    RateLimiting: added basic rate liimiting with upstash/redis.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
 import  { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
-
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -11,15 +11,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// Connect to Database
-connectDB();
+
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use(rateLimiter);
+
 
 app.use("/api/notes", notesRoutes);
 
-
+// Connect to Database
+connectDB().then(() => {
+    console.log("Connected to the database");   
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
 });
